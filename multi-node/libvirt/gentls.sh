5a82c567fe84bee99c7593cc5aae0118497d40c8
George Tankersley <george.tankersley@gmail.com>
2015-11-09 17:24:22 -0800

    multi-node/libvirt: add cluster IP to apiserver cert

--- multi-node/libvirt/gentls.sh
+++ multi-node/libvirt/gentls.sh
@@ -8,6 +8,8 @@ fi
 echo "Generating TLS assets..."
 mkdir -p certs
 ../../lib/init-ssl-ca ./certs
-../../lib/init-ssl ./certs apiserver kube-apiserver IP.1=127.0.0.1,IP.2=192.168.120.10
+
+# the apiserver can be addressed locally, over the NAT bridge, or by internal cluster IP
+../../lib/init-ssl ./certs apiserver kube-apiserver IP.1=127.0.0.1,IP.2=192.168.120.10,IP.3=10.3.0.1
 ../../lib/init-ssl ./certs worker kube-worker IP.1=127.0.0.1,IP.2=192.168.120.11
 ../../lib/init-ssl ./certs admin kube-admin
